9d6a83125da3c0ca93fca4a34e3378d450c81254
Jonathan Ahrens <jahrens@harthowerton.com>
2022-10-28 17:34:46 -0700

    Search O365 Forwarding Rules and filter/delete

---
+++ Search Office 365 User Forwarding Rule.ps1
@@ -0,0 +1,19 @@
+Connect-ExchangeOnline 
+
+$UserID = Read-Host -Prompt "Enter User's email"
+
+Get-InboxRule -Mailbox $UserID | Select-Object Description,Enabled,Identity,RuleIdentity | Export-Csv C:\path\to\list.csv
+
+foreach ($rule in $(Get-InboxRule -Mailbox $UserID){
+
+    Write-Host $Rule.Name
+    Write-Host $Rule.Description
+
+    $Prompt = Read-Host -Prompt "Remove $($Rule.Name)? (y/N)"
+
+    if ($Prompt -like 'y'){
+        Remove-InboxRule -Identity $Rule.RuleIdentity
+    }
+
+    Start-Sleep -Seconds 1    
+}
