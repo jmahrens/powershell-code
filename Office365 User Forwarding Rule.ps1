be3e23155c6a3a04c715298339e42cf9d191fdcc
Jonathan Ahrens <jahrens@harthowerton.com>
2022-10-28 17:45:11 -0700

    Fixed closing bracket

--- Office365 User Forwarding Rule.ps1
+++ Office365 User Forwarding Rule.ps1
@@ -4,8 +4,7 @@ $UserID = Read-Host -Prompt "Enter User's email"
 
 Get-InboxRule -Mailbox $UserID | Select-Object Description,Enabled,Identity,RuleIdentity | Export-Csv C:\path\to\list.csv
 
-foreach ($rule in $(Get-InboxRule -Mailbox $UserID){
-
+foreach ($rule in $(Get-InboxRule -Mailbox $UserID)){
     Write-Host $Rule.Name
     Write-Host $Rule.Description
 
@@ -15,5 +14,5 @@ foreach ($rule in $(Get-InboxRule -Mailbox $UserID){
         Remove-InboxRule -Identity $Rule.RuleIdentity
     }
 
-    Start-Sleep -Seconds 1    
+    Start-Sleep -Seconds 1 
 }
